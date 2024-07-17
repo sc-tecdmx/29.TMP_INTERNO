22db4543c779e2f049d7b907525cac09a5bec3fb
ggarciab <=>
2024-07-17 17:03:47 -0600

    base

---
+++ src/main/java/mx/gob/tecdmx/directorio_interno/DirectorioInternoApplication.java
@@ -0,0 +1,13 @@
+package mx.gob.tecdmx.directorio_interno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DirectorioInternoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DirectorioInternoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/gob/tecdmx/directorio_interno/DirectorioInternoApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.gob.tecdmx.directorio_interno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DirectorioInternoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
